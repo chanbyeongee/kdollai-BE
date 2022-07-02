431bf550cc959c33c5b5a2f1aad00a42d110d546
ChanByeongee <well87865@gmail.com>
2022-07-03 00:27:45 +0900

    Fix some Bugs

--- packages/k_doll_ai_chatbot/__init__.py
+++ packages/k_doll_ai_chatbot/__init__.py
@@ -1,4 +1,4 @@
-from setup import setup_environ, download_weights
+from .setup import setup_environ, download_weights
 from .transformer_models.aimodel import AIModel
 __all__=['tensormodels','tokenizer', 'AIModel']
 
